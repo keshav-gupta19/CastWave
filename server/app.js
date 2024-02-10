e69ba05b96e304dff67cdb8cc891f44a28b1cb74
Keshav Gupta <106925920+keshav-gupta19@users.noreply.github.com>
2024-02-10 14:34:45 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -28,7 +28,12 @@ app.use((err, req, res, next) => {
 });
 // console.log(process.env.JWT_KEY);
 app.use(express.json({ limit: "50mb" }));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 const corsConfig = {
   origin: ["https://cast-wave-keshav.vercel.app/"],
   methods: ["POST", "GET", "PUT"],
